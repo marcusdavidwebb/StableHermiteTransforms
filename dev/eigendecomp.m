f37b9d5b0290f0e4ec31c130e041f82cde13c706
GeorgAUT <gam37@cam.ac.uk>
2026-03-13 16:50:19 +0000

    sparse vs dense eig solve

---
+++ dev/eigendecomp.m
@@ -0,0 +1,38 @@
+clear all;
+clc;
+%% Parameters
+jmax=10;
+cost_sparse=zeros(jmax,1);
+cost_dense=zeros(jmax,1);
+
+%% Compare cost
+
+
+for j=1:10
+    n=2^j;
+    a=sqrt((1:(n-1))'/2)
+    A=spdiags(a,-1,n,n);
+    A=A+A';
+    
+    tic
+    [V,D]=eigs(A,n);
+    cost_sparse(j)=toc;
+
+    tic
+    [V,D]=eig(full(A));
+    cost_dense(j)=toc;
+
+
+end
+
+%% Plot results
+
+semilogy(2.^(1:jmax),cost_sparse)
+hold on
+semilogy(2.^(1:jmax), cost_dense);
+xlabel('Matrix Size (n)');
+ylabel('Computation Time (seconds)');
+legend('Sparse', 'Dense');
+title('Comparison of Sparse and Dense Eigenvalue Computation');
+grid on;
+hold off;

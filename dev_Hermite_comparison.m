f28e8b6448c1935f93c938e994cbace60827a07a
GeorgAUT <gam37@cam.ac.uk>
2026-03-14 10:22:04 +0000

    working direct code

--- dev_Hermite_comparison.m
+++ dev_Hermite_comparison.m
@@ -6,10 +6,17 @@ addpath('modules/quadrature/')
 addpath('modules/')
 
 %% 
-N=11;
+N=10;
 
 [x,w]=quad_gauss_hermite(N); % quadrature points and nodes
 [T,Tinv]=initialise_Hermite_transform_direct(N);
+[d1,Q1]=initialise_Hermite_transform2(N);
+
+norm(diag(d1)*Q1'-T)
+
+norm(Q1*diag(1./d1)-Tinv)
+
+norm(T*Tinv-eye(N))
 
 % [d,Q]=initialise_Hermite_transform_old(x);
 % a=hermpts(N)
@@ -20,9 +27,8 @@ N=11;
 % norm(x-hermpts(N))
 
 
-[d1,Q1]=initialise_Hermite_transform2(300);
-
-A=Q1'*Q1;
-
-diag(A)
+% 
+% A=Q1'*Q1;
+% 
+% diag(A)
 % A(end,end)

cf8139e56d2a3bdd441b8bd7d9d604473bd3244b
GeorgAUT <gam37@cam.ac.uk>
2026-03-20 23:43:28 +0000

    minor updates change x-axis

--- performance_evals_plot.m
+++ performance_evals_plot.m
@@ -11,15 +11,16 @@ load("data/numerical_eval_N_multiple_transforms.mat")
 %% Plot 1: Assembly time
 clear h
 figure(1)
-h(3)=semilogy(N_vec,time_GW,'-','Color','blue','LineWidth',3,'MarkerFaceColor','white')
+h(3)=loglog(N_vec,time_GW,'-','Color','blue','LineWidth',3,'MarkerFaceColor','white')
 hold on
-h(2)=semilogy(N_vec,time_B,'-.','Color','#edb120','LineWidth',3,'MarkerFaceColor','white')
-h(1)=semilogy(N_vec,time_direct,'--','Color','#c74cb9','LineWidth',3,'MarkerFaceColor','white')%,'Color','#edb120','LineWidth',2)
+h(2)=loglog(N_vec,time_B,'-.','Color','#edb120','LineWidth',3,'MarkerFaceColor','white')
+h(1)=loglog(N_vec,time_direct,'--','Color','#c74cb9','LineWidth',3,'MarkerFaceColor','white')%,'Color','#edb120','LineWidth',2)
+h(4)=loglog(N_vec,N_vec.^2*1e-6,'-.','Color','red','LineWidth',1,'MarkerFaceColor','white')%,'Color','#edb120','LineWidth',2)
 set(gca,'FontSize',16)
 ylabel('CPU time (sec)','Interpreter','latex', 'FontSize', 22)
 xlabel('$N$','Interpreter','latex', 'FontSize', 22)
 
-legend(h,'Direct', 'Bunck', 'Golub--Welsch','Interpreter','latex', 'FontSize', 16,'Location','northwest')
+legend(h,'Direct', 'Bunck', 'Golub--Welsch','$\mathcal{O}(N^2)$','Interpreter','latex', 'FontSize', 16,'Location','northwest')
 grid on
 hold off
 exportgraphics(gcf,strcat("images/assembly_time_multiple.pdf"),'ContentType','vector')
@@ -28,10 +29,10 @@ exportgraphics(gcf,strcat("images/assembly_time_multiple.pdf"),'ContentType','ve
 %% Plot 2: Error in T
 clear h
 figure(2)
-h(3)=semilogy(N_vec,T_error_GW,'-','Color','blue','LineWidth',3,'MarkerFaceColor','white')
+h(3)=loglog(N_vec,T_error_GW,'-','Color','blue','LineWidth',3,'MarkerFaceColor','white')
 hold on
-h(2)=semilogy(N_vec,T_error_B,'-.','Color','#edb120','LineWidth',3,'MarkerFaceColor','white')
-h(1)=semilogy(N_vec,T_error_direct,'--','Color','#c74cb9','LineWidth',3,'MarkerFaceColor','white')%,'Color','#edb120','LineWidth',2)
+h(2)=loglog(N_vec,T_error_B,'-.','Color','#edb120','LineWidth',3,'MarkerFaceColor','white')
+h(1)=loglog(N_vec,T_error_direct,'--','Color','#c74cb9','LineWidth',3,'MarkerFaceColor','white')%,'Color','#edb120','LineWidth',2)
 set(gca,'FontSize',16)
 ylabel('$\|T-T_{\mathrm{approx}}\|_2$','Interpreter','latex', 'FontSize', 22)
 xlabel('$N$','Interpreter','latex', 'FontSize', 22)
@@ -45,10 +46,10 @@ exportgraphics(gcf,strcat("images/error_T_multiple.pdf"),'ContentType','vector')
 %% Plot 3: Error in Tinv
 clear h
 figure(3)
-h(3)=semilogy(N_vec,Tinv_error_GW,'-','Color','blue','LineWidth',3,'MarkerFaceColor','white')
+h(3)=loglog(N_vec,Tinv_error_GW,'-','Color','blue','LineWidth',3,'MarkerFaceColor','white')
 hold on
-h(2)=semilogy(N_vec,Tinv_error_B,'-.','Color','#edb120','LineWidth',3,'MarkerFaceColor','white')
-h(1)=semilogy(N_vec,Tinv_error_direct,'--','Color','#c74cb9','LineWidth',3,'MarkerFaceColor','white')%,'Color','#edb120','LineWidth',2)
+h(2)=loglog(N_vec,Tinv_error_B,'-.','Color','#edb120','LineWidth',3,'MarkerFaceColor','white')
+h(1)=loglog(N_vec,Tinv_error_direct,'--','Color','#c74cb9','LineWidth',3,'MarkerFaceColor','white')%,'Color','#edb120','LineWidth',2)
 set(gca,'FontSize',16)
 ylabel('$\|T^{-1}-T_{\mathrm{approx}}^{-1}\|_2$','Interpreter','latex', 'FontSize', 22)
 xlabel('$N$','Interpreter','latex', 'FontSize', 22)
@@ -62,10 +63,10 @@ exportgraphics(gcf,strcat("images/error_Tinv_multiple.pdf"),'ContentType','vecto
 %% Plot 4: Condition number of T
 clear h
 figure(4)
-h(3)=semilogy(N_vec,cond_GW,'-','Color','blue','LineWidth',3,'MarkerFaceColor','white')
+h(3)=loglog(N_vec,cond_GW,'-','Color','blue','LineWidth',3,'MarkerFaceColor','white')
 hold on
-h(2)=semilogy(N_vec,cond_B,'-.','Color','#edb120','LineWidth',3,'MarkerFaceColor','white')
-h(1)=semilogy(N_vec,cond_direct,'--','Color','#c74cb9','LineWidth',3,'MarkerFaceColor','white')%,'Color','#edb120','LineWidth',2)
+h(2)=loglog(N_vec,cond_B,'-.','Color','#edb120','LineWidth',3,'MarkerFaceColor','white')
+h(1)=loglog(N_vec,cond_direct,'--','Color','#c74cb9','LineWidth',3,'MarkerFaceColor','white')%,'Color','#edb120','LineWidth',2)
 set(gca,'FontSize',16)
 ylabel('$\mathrm{cond}(T)$','Interpreter','latex', 'FontSize', 22)
 xlabel('$N$','Interpreter','latex', 'FontSize', 22)

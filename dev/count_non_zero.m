31b6cec50f5798ed940f752062469c867027e462
GeorgAUT <gam37@cam.ac.uk>
2026-04-07 21:24:33 +0100

    count number of zeros in Q

---
+++ dev/count_non_zero.m
@@ -0,0 +1,49 @@
+%% Performance metrics of three approaches to computing the Hermite transform
+clear all
+rng(1) % Fix random seed for reproducability
+%% Compare direct assembly of Hermite transform against stable one
+
+addpath('modules/quadrature/')
+addpath('modules/')
+
+%% Initialise performance measures
+N_vec=[4,6,8,11,16,23,32,45,64,91,128,181,256,362,512,724,1024,1448,2048,2896,4096]; % transform sizes for experiments
+zerovec=zeros(size(N_vec));
+
+count=zerovec;
+
+%% Evaluate the performance tests for various transform sizes
+for jj=1:max(size(N_vec))
+    jj
+    [d_GW, Q_GW] = initialise_Hermite_transform_Golub_Welsch(N_vec(jj));
+    tol = 1e-10;              % choose a tolerance
+    count(jj) = sum(abs(Q_GW(:)) < tol);
+    % count(jj)=nnz(Q_GW);
+end
+
+%% Plotting
+figure(1)
+loglog(N_vec,count,'b-','LineWidth',2)
+hold on
+loglog(N_vec,N_vec.^1.5,'r-.','LineWidth',2)
+loglog(N_vec,N_vec.^2,'r-.','LineWidth',2)
+
+legend('\# $|Q_{ij}|<10^{-10}$','$N^{1.5}, N^2$','Interpreter','latex', 'FontSize', 16,'Location','northwest')
+grid on
+hold off
+
+exportgraphics(gcf,strcat("../dev/num_nonzeroQ.pdf"),'ContentType','vector')
+
+
+%% Plotting
+figure(2)
+loglog(N_vec,N_vec.^2-count,'b-','LineWidth',2)
+hold on
+loglog(N_vec,N_vec.^1.5,'r-.','LineWidth',2)
+loglog(N_vec,N_vec.^2,'r-.','LineWidth',2)
+
+legend('\# $|Q_{ij}|>10^{-10}$','$N^{1.5}, N^2$','Interpreter','latex', 'FontSize', 16,'Location','northwest')
+grid on
+hold off
+
+exportgraphics(gcf,strcat("../dev/num_zeroQ.pdf"),'ContentType','vector')
